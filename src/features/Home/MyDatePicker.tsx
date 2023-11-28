db7000c7ee31ea2bf9ca689ab0a79e8faccbe93f
김봉준 <nocdu112@naver.com>
2023-11-28 22:28:07 +0900

    에러수정

--- src/features/Home/MyDatePicker.tsx
+++ src/features/Home/MyDatePicker.tsx
@@ -1,25 +1,25 @@
-import styled from '@emotion/styled';
-import SelectBox from '@/components/common/SelectBox/SelectBox';
+// import styled from '@emotion/styled';
+// import SelectBox from '@/components/common/SelectBox/SelectBox';
 
-const MyDatePicker = () => {
-  return <SelectBox />;
-};
+// const MyDatePicker = () => {
+//   return <SelectBox />;
+// };
 
-export default MyDatePicker;
+// export default MyDatePicker;
 
-const Wrapper = styled.div`
-  padding: 12px 8px;
-  position: relative;
-`;
+// const Wrapper = styled.div`
+//   padding: 12px 8px;
+//   position: relative;
+// `;
 
-const EmergencyDispatch = styled.div`
-  display: flex;
-  padding: 16px 12px;
-  justify-content: space-between;
-  align-items: center;
-  flex: 1 0 0;
-  align-self: stretch;
-  border-radius: 4px;
-  border: 1px solid #e9ecef;
-  background: #f8f9fa;
-`;
+// const EmergencyDispatch = styled.div`
+//   display: flex;
+//   padding: 16px 12px;
+//   justify-content: space-between;
+//   align-items: center;
+//   flex: 1 0 0;
+//   align-self: stretch;
+//   border-radius: 4px;
+//   border: 1px solid #e9ecef;
+//   background: #f8f9fa;
+// `;
