2c86c83db44df2415867edc99fd9382af135a3a5
kimbongjune <nocdu112@naver.com>
2023-12-19 17:09:09 +0900

    메뉴탭 재난 진행상태 표기 변경

--- src/features/Home/EventStatus.tsx
+++ src/features/Home/EventStatus.tsx
@@ -26,12 +26,12 @@ const EventStatus = (props: Props) => {
 
   return (
     <Stack spacing="4px" align="center">
-      {props.reportCount === 1 && (
+      {props.reportCount === 0 && (
         <Circle eventType={props.eventType} status={props.status}>
           <PhoneInTalk width={24} height={24} />
         </Circle>
       )}
-      {props.reportCount > 1 && props.reportCount <= 5 && <Circle eventType={props.eventType}>{props.reportCount}</Circle>}
+      {props.reportCount >= 1 && props.reportCount <= 5 && <Circle eventType={props.eventType}>{props.reportCount}</Circle>}
       {props.reportCount > 5 && <Circle eventType={props.eventType}>5+</Circle>}
       {props.status === 'progress' && (
         <Text eventType={props.eventType} status={props.status}>
